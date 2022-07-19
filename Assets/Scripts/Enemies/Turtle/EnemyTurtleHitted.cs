95ac50066899f861659cbbe21b15f300dbdaf331
Axel <55245056+Hareyuk@users.noreply.github.com>
2022-07-19 02:48:50 -0300

    ENemigo empujadoo

--- Assets/Scripts/Enemies/Turtle/EnemyTurtleHitted.cs
+++ Assets/Scripts/Enemies/Turtle/EnemyTurtleHitted.cs
@@ -9,12 +9,20 @@ public class EnemyTurtleHitted : EnemyTurtleState
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        enemy = GetComponent<EnemyTurtle>(); //Needed to fix a bug
     }
     public override void Init()
     {
-        print("Hitted");
         enemy.anim.Play("GetHit");
-        Vector3 moveDirection = rb.transform.position;
-        rb.AddForce(moveDirection.normalized * -500f);
+        //Vector3 moveDirection = rb.transform.position - Vector3.back;
+        Vector3 moveDirection = rb.transform.position - enemy.character.transform.position;
+        moveDirection.y = 0;
+        moveDirection = moveDirection.normalized;
+        rb.AddForce(moveDirection * 500f * distanceHitted);
+    }
+
+    public void returnToIdle()
+    {
+        enemy.SetNewState(states.IDLE);
     }
 }
